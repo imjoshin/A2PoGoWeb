8010f17b34d9b9c6c97bf1bfd154e9c99fb29416
Josh <jocjohns@mtu.edu>
2017-08-26 11:43:17 -0400

    Added fontawesome

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
 <head>
   <title>A2 Pokemon Go</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=0">
+  <link href="dist/css/font-awesome.min.css" rel="stylesheet" />
   <link href="dist/css/app.css" rel="stylesheet" media="screen" />
   <script src="dist/js/jquery-3.2.1.min.js"></script>
   <script src="dist/js/app.js"></script>
