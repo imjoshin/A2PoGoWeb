8116686e16a906af7c70035883119edf3d4efc28
Josh <jocjohns@mtu.edu>
2017-08-26 11:39:22 -0400

    Added md5 encryption to passwords

--- assets/js/login.js
+++ assets/js/login.js
@@ -13,35 +13,51 @@ $().ready(function() {
 	$('.login-modal-form .btn').on('click', function() {
 		var form = $(this).parent();
 		var formType = form.parent().data('form');
+		var formData = [];
 
 		$.each(form.find('input'), function() {
 			if(!$(this).val().trim().length) {
 				$(this).addClass('input-error');
 			} else {
 				$(this).removeClass('input-error');
+				formData[$(this).attr('name')] = $(this).val();
 			}
 		});
 
-		if (!form.find('input-error').length) {
-			$.ajax({
-				type: "POST",
-				dataType: "json",
-				url: "php/ajax.php",
-				data: {
-					call: formType,
-					form: form.serialize()
-				},
-				success: function(data) {
-					if (data.success) {
-						$('.login-modal .message').empty();
-						location.reload();
-					} else {
-						$('.login-modal .message').addClass('message-error');
-						$('.login-modal .message').html(data.output);
-					}
-				}
-			});
+		if (form.find('.input-error').length) {
+			return;
 		}
+
+		if (formType == 'register') {
+			console.log(formData['password'].length);
+			if (formData['password'].length < 6 || formData['password'].length > 32 || /\s/g.test(formData['password'])) {
+				$('.login-modal .message').addClass('message-error');
+				$('.login-modal .message').html("Invalid password.");
+			}
+
+			formData['confirm_password'] = MD5(formData['confirm_password']);
+		}
+
+		formData['password'] = MD5(formData['password']);
+
+		$.ajax({
+			type: "POST",
+			dataType: "json",
+			url: "php/ajax.php",
+			data: {
+				call: formType,
+				form: serializeArray(formData)
+			},
+			success: function(data) {
+				if (data.success) {
+					$('.login-modal .message').empty();
+					location.reload();
+				} else {
+					$('.login-modal .message').addClass('message-error');
+					$('.login-modal .message').html(data.output);
+				}
+			}
+		});
 	});
 
 	$('.logout').on('click', function() {
