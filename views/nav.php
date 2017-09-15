3c7aa396781d438aa117a012a1d021c4f7daa699
Josh <jocjohns@mtu.edu>
2017-09-15 00:03:42 -0400

    Implemented scrollbar for nav

--- views/nav.php
+++ views/nav.php
@@ -9,55 +9,57 @@
 					Maps
 				</div>
 			</div>
-			<div class="nav-tabs-container nav-tabs-container--active" data-tab="accounts">
-				<?php
-					foreach ($accounts as $account)
-					{
-						echo "
-						<div class='row grid-x nav-tabs-container-item " . ($account['id'] < 0 ? 'nav-tabs-container-item-template' : '') . "' data-id='{$account['id']}' data-fields='" . json_encode($account) . "'>
-							<div class='columns small-1 nav-tabs-container-item-icon'>
-								<i class='fa {$account['icon']}' aria-hidden='true'></i>
-							</div>
-							<div class='columns small-11 nav-tabs-container-item-info'>
-								<div class='row nav-tabs-container-item-info-name'>
-									<label class='account-name'>{$account['name']}</label>
+			<div class="nav-scrollable">
+				<div class="nav-tabs-container nav-tabs-container--active" data-tab="accounts">
+					<?php
+						foreach ($accounts as $account)
+						{
+							echo "
+							<div class='row grid-x nav-tabs-container-item " . ($account['id'] < 0 ? 'nav-tabs-container-item-template' : '') . "' data-id='{$account['id']}' data-fields='" . json_encode($account) . "'>
+								<div class='columns small-1 nav-tabs-container-item-icon'>
+									<i class='fa {$account['icon']}' aria-hidden='true'></i>
 								</div>
-								<div class='row nav-tabs-container-item-info-details'>
-									<label class='account-address'>{$account['detail']}</label>
+								<div class='columns small-11 nav-tabs-container-item-info'>
+									<div class='row nav-tabs-container-item-info-name'>
+										<label class='account-name'>{$account['name']}</label>
+									</div>
+									<div class='row nav-tabs-container-item-info-details'>
+										<label class='account-address'>{$account['detail']}</label>
+									</div>
 								</div>
-							</div>
-						</div>";
-					}
-				?>
-				<div class='row grid-x nav-tabs-container-buttons'>
-					<div class='columns small-12'>
-						<span class="btn btn-white btn-add" data-name="Add Account">Add Account</span>
+							</div>";
+						}
+					?>
+					<div class='row grid-x nav-tabs-container-buttons'>
+						<div class='columns small-12'>
+							<span class="btn btn-white btn-add" data-name="Add Account">Add Account</span>
+						</div>
 					</div>
 				</div>
-			</div>
-			<div class="nav-tabs-container" data-tab="maps">
-				<?php
-					foreach ($maps as $map)
-					{
-						echo "
-						<div class='row grid-x nav-tabs-container-item " . ($map['id'] < 0 ? 'nav-tabs-container-item-template' : '') . "' data-id='{$map['id']}' data-fields='" . json_encode($map) . "'>
-							<div class='columns small-1 nav-tabs-container-item-icon'>
-								<i class='fa {$map['icon']}' aria-hidden='true'></i>
-							</div>
-							<div class='columns small-11 nav-tabs-container-item-info'>
-								<div class='row nav-tabs-container-item-info-name'>
-									<label class='account-name'>{$map['name']}</label>
+				<div class="nav-tabs-container" data-tab="maps">
+					<?php
+						foreach ($maps as $map)
+						{
+							echo "
+							<div class='row grid-x nav-tabs-container-item " . ($map['id'] < 0 ? 'nav-tabs-container-item-template' : '') . "' data-id='{$map['id']}' data-fields='" . json_encode($map) . "'>
+								<div class='columns small-1 nav-tabs-container-item-icon'>
+									<i class='fa {$map['icon']}' aria-hidden='true'></i>
 								</div>
-								<div class='row nav-tabs-container-item-info-details'>
-									<label class='account-address'>{$map['detail']}</label>
+								<div class='columns small-11 nav-tabs-container-item-info'>
+									<div class='row nav-tabs-container-item-info-name'>
+										<label class='account-name'>{$map['name']}</label>
+									</div>
+									<div class='row nav-tabs-container-item-info-details'>
+										<label class='account-address'>{$map['detail']}</label>
+									</div>
 								</div>
-							</div>
-						</div>";
-					}
-				?>
-				<div class='row grid-x nav-tabs-container-buttons'>
-					<div class='columns small-12'>
-						<span class="btn btn-white btn-add" data-name="Add Map">Add Map</span>
+							</div>";
+						}
+					?>
+					<div class='row grid-x nav-tabs-container-buttons'>
+						<div class='columns small-12'>
+							<span class="btn btn-white btn-add" data-name="Add Map">Add Map</span>
+						</div>
 					</div>
 				</div>
 			</div>
